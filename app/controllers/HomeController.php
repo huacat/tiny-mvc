15102e61559043e38b8c10ff525e677c7ecbecac
huacat <huacat@qq.com>
2018-04-28 17:09:28 +0800

    add redis vender and service

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -3,14 +3,17 @@
 class HomeController extends BaseController{
 
     public function home(){
-        $this->view = View::make('home')
-            ->with('article',Article::first())
-            ->withTitle('tiny-mvc');
+//        $this->view = View::make('home')
+//            ->with('article',Article::first())
+//            ->withTitle('tiny-mvc');
 
-        $this->mail = Mail::to(['295806460@qq.com'])
-            ->from('huacat <cvisix@163.com>')
-            ->title('分支合并')
-            ->content('<h1>Branch Merge</h1><p>Merge dev to master and push to origin/master!</p>');
+//        $this->mail = Mail::to(['295806460@qq.com'])
+//            ->from('huacat <cvisix@163.com>')
+//            ->title('分支合并')
+//            ->content('<h1>Branch Merge</h1><p>Merge dev to master and push to origin/master!</p>');
+
+        Redis::set('name','yi',5,'s');
+        var_dump(Redis::get('name'));
 
     }
 }
